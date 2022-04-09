a4fcf1f53b2ff5fa0cb1af451ce42782800c5128
focus61 <focussashka161@gmail.com>
2022-04-09 19:25:59 +0300

    _

--- Photo App/Extensions/Extension_UIView.swift
+++ Photo App/Extensions/Extension_UIView.swift
@@ -1,8 +1,6 @@
 import UIKit
 extension UIView {
-    enum ViewSide {
-        case Left, Right, Top, Bottom
-    }
+
     
     func addSubviews(_ views: UIView...) {
         for i in views {
@@ -25,5 +23,23 @@ extension UIView {
         let alertAct = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
         alertCont.addAction(alertAct)
         target.present(alertCont, animated: true, completion: nil)
+    }
+    
+    enum ViewSide {
+        case Left, Right, Top, Bottom
+    }
+    
+    func addBorder(toSide side: ViewSide, withColor color: CGColor, andThickness thickness: CGFloat) {
+        
+        let border = CALayer()
+        border.backgroundColor = color
+        
+        switch side {
+        case .Left: border.frame = CGRect(x: frame.minX, y: frame.minY, width: thickness, height: frame.height); break
+        case .Right: border.frame = CGRect(x: frame.maxX, y: frame.minY, width: thickness, height: frame.height); break
+        case .Top: border.frame = CGRect(x: frame.minX, y: frame.minY, width: frame.width, height: thickness); break
+        case .Bottom: border.frame = CGRect(x: frame.minX, y: frame.maxY, width: frame.width, height: thickness); break
+        }
+        layer.addSublayer(border)
     }
 }
